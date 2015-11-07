7e7f862634a806f963971110b110dee9d85a3f12
Nick Strupat <nick.strupat@gmail.com>
2015-11-07 13:54:16 -0500

    working implementation

--- caligned_malloc.c
+++ caligned_malloc.c
@@ -1,2 +1,11 @@
-#include "aligned_malloc.h"
+#include "AlignedMalloc/aligned_malloc.h"
+#include "CacheLineSize/CacheLineSize.h"
 
+#include <stddef.h>
+
+void * caligned_malloc(size_t size) {
+	static size_t cache_line_size = -1;
+	if (cache_line_size == -1)
+		cache_line_size = CacheLineSize();
+	return aligned_malloc(size, cache_line_size);
+}
